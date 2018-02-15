8fdc19a0ad8d7eaf29c8b5624d0a224698dddf9f
hailey0306 <36516720+hailey0306@users.noreply.github.com>
2018-02-15 11:20:41 -0800

    Assignment 1 & Extra Credit

---
+++ loan-calculator.js
@@ -0,0 +1,24 @@
+
+require('mahrio').runServer( process.env, __dirname)
+    .then( function(server){
+        server.route({
+            path: '/',
+            method: 'GET',
+            handler: function(req, rep){
+                rep('loan calculator');
+            }
+        });
+
+        server.route({
+            path: '/loan',
+            method: 'POST',
+            handler: function(req, rep){ // req.payload.motion&night
+                var loanAmount=req.payload.loanAmount;
+                var yearsToPay=req.payload.yearsToPay;
+                var interestRate=(req.payload.interestRate)/100;
+                var monthlyPayment=(interestRate/12*loanAmount)/(1-Math.pow((1+(interestRate/12)),0-12*yearsToPay));
+                rep('Your monthly payment will be : $'+monthlyPayment.toFixed()+'.');
+
+            }
+        });
+    });
